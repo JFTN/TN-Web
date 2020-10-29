5ca99c62967a4b29603ec19d485da72c3e6d9629
Sophie <sophiewongsp@gmail.com>
2020-10-29 19:36:44 +0800

    ✨FEAT (SignIn): add css to SignIn page
    - body format
    - padding to components
    - height & width changes

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react"; //, { useState } from 'react';
+import "../styles/SignIn.css"
+
+// const [username, setUsername] = useState();
+// const [password, setPassword] = useState();
 
 var username;
 var password;
 
+// password visibility
+// const togglePasswordVisiblity = () => {
+//   setPasswordShown(passwordShown ? false : true);
+// };
+
+// setting variables
 function SetUsername(props){
   username = props.target.value;
 }
@@ -11,6 +21,7 @@ function SetPassword(props){
   password = props.target.value;
 }
 
+// submit form
 function handleSubmit(event){
   event.preventDefault();
 }
@@ -18,19 +29,25 @@ function handleSubmit(event){
 const SignInPage = () => {
   return (
     <div>
-      <h1>Sign In page</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-         Username: 
-          <input autoFocus type="text" value={username} onChange={SetUsername}/>
-        </label>
+      <h1 className="header">Sign In</h1>
+      <form onSubmit={handleSubmit} className="signIn">
+        <div className="inputSection">
+          <label>
+            {/* Username:  */}
+            <input autoFocus type="text" placeholder="Username" value={username} onChange={SetUsername} 
+            className="input" />
+          </label>
+        </div>
         <br/>
-        <label>
-          Password: 
-          <input type="password" value={password} onChange={SetPassword}/>
-        </label>
+        <div className="inputSection">
+          <label>
+            {/* Password:  */}
+            <input type="password" placeholder="Password" value={password} onChange={SetPassword} 
+            className="input" />
+          </label>
+        </div>
         <br/>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Sign In" className="button"/>
       </form>
     </div>
   );
