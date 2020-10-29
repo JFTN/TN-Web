b5c51c328d9b99f33f47c9fbfb50ac81e21aaaaf
James <jameslimbj@gmail.com>
2020-10-29 22:14:06 +0800

    :hammer: REFACTOR (signIn): migrate sign in page from functional to class

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; //, { useState } from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/SignIn.css";
 
@@ -9,76 +9,85 @@ var capsLockWarning = "";
 //   setPasswordShown(passwordShown ? false : true);
 // };
 
-// setting variables
+class SignInPage extends React.Component {j
 
-// submit form
-function onSubmit(event) {
-  event.preventDefault();
-}
-
-// detect caps lock
-function onKeyDown(event) {
-  if (event.keyCode == 9) {
-    capsLockWarning = "Caps Lock is On!";
-    console.log("caps lock");
-  } else {
-    capsLockWarning = "";
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
-}
 
-const onChange = (setter) => (event) => {
-  setter(event.nativeEvent.target.value);
-};
+  render() {
+    return (
+      <div>
+        <h1 className="header">Sign In</h1>
 
-const SignInPage = () => {
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
-
-  return (
-    <div>
-      <h1 className="header">Sign In</h1>
-
-      <Link to="/SignUp">
-        <button className="signUpButton" type="button">
-          Sign Up
-        </button>
-      </Link>
-
-      <form className="signIn" onSubmit={onSubmit}>
-        <div className="inputSection">
-          <input
-            autoFocus
-            className="input"
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={onChange(setUsername)}
-          />
-        </div>
-        <br />
-        <div className="inputSection">
-          {capsLockWarning}
-          <input
-            className="input"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={onChange(setPassword)}
-            onKeyDown={onKeyDown}
-          />
-        </div>
-        <br />
-        <input 
-          className="button" 
-          type="submit" 
-          value="Sign In" 
-        />
-        <Link className="forgotPassword" to="/forgotPassword">
-          <p>Forgot Password?</p>
+        <Link to="/SignUp">
+          <button className="signUpButton" type="button">
+            Sign Up
+          </button>
         </Link>
-      </form>
-    </div>
-  );
+
+        <form className="signIn" onSubmit={this.onSubmit}>
+          <div className="inputSection">
+            <input
+              autoFocus
+              className="input"
+              type="text"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={event => {                  
+                this.setState({         
+                  username: event.nativeEvent.target.value
+                });
+              }}
+            />
+          </div>
+          <br />
+          <div className="inputSection">
+            {capsLockWarning}
+            <input
+              className="input"
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={event => {
+                this.setState({         
+                  username: event.nativeEvent.target.value
+                });
+              }}
+              onKeyDown={this.onKyDown}
+            />
+          </div>
+          <br />
+          <input 
+            className="button" 
+            type="submit" 
+            value="Sign In" 
+          />
+          <Link className="forgotPassword" to="/forgotPassword">
+            <p>Forgot Password?</p>
+          </Link>
+        </form>
+      </div>
+    );
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  // detect caps lock
+  onKeyDown = (event) => {
+    if (event.keyCode == 9) {
+      capsLockWarning = "Caps Lock is On!";
+      console.log("caps lock");
+    } else {
+      capsLockWarning = "";
+    }
+  }
 };
 
 export default SignInPage;
