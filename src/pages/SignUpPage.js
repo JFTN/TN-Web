671e71c36e2a787594e90514b1e8aebc0107bfdb
Miahcasso <42335393+Miahcasso@users.noreply.github.com>
2020-10-29 21:26:36 +0800

    Revert "feat (SignUp): barebone ui elements"
    This reverts commit 8271eb956214477b7af281ad62604ea706728d24.

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -4,33 +4,6 @@ const SignUpPage = () => {
   return (
     <div>
       <h1>Sign Up page</h1>
-      <div>
-        <label for="email">Email</label>
-        <br></br>
-        <input type="email"></input>
-
-        <br></br>
-
-        <label for="username">Username</label>
-        <br></br>
-        <input type="text"></input>
-
-        <br></br>
-
-        <label for="password">Password</label>
-        <br></br>
-        <input type="password"></input>
-
-        <br></br>
-
-        <label for="confirm-password">Confirm Password</label>
-        <br></br>
-        <input type="password"></input>
-
-        <br></br>
-        
-        <input type="submit" value="Submit"></input>
-      </div>
     </div>
   );
 }
